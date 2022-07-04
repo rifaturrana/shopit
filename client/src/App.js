79df31d45f3728aead7d782059aa92f205f85d19
rifaturrana <smrifaturrana@gmail.com>
2022-07-05 01:12:43 +0600

    title

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+import Home from "./components/Home";
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="container container-fluid">
+          <Route path="/" element={<Home />} exact />
+        </div>
+        <Footer />
+      </div>
+    </Router>
+    // <Home />
+  );
+}
+
+export default App;
